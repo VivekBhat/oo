6a5286776d0c54c7859490ce452ce4a864fdca81
Samrudhi <ssharm17@ncsu.edu>
2015-10-08 16:20:19 -0400

    change users.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925001759) do
+ActiveRecord::Schema.define(version: 20151007235503) do
 
   create_table "books", force: :cascade do |t|
     t.string   "ISBN",        limit: 255
@@ -21,7 +21,13 @@ ActiveRecord::Schema.define(version: 20150925001759) do
     t.string   "status",      limit: 255
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.boolean  "check"
+    t.integer  "want_book",   limit: 4
+    t.integer  "user_id",     limit: 4
+    t.string   "email",       limit: 255
   end
+
+  add_index "books", ["user_id"], name: "index_books_on_user_id", using: :btree
 
   create_table "histories", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -35,6 +41,16 @@ ActiveRecord::Schema.define(version: 20150925001759) do
     t.string   "status",      limit: 255
   end
 
+  create_table "suggestions", force: :cascade do |t|
+    t.string   "ISBN",        limit: 255
+    t.string   "author",      limit: 255
+    t.text     "description", limit: 65535
+    t.string   "title",       limit: 255
+    t.string   "status",      limit: 255
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name",                  limit: 255
     t.string   "email",                 limit: 255
@@ -45,6 +61,10 @@ ActiveRecord::Schema.define(version: 20150925001759) do
     t.datetime "updated_at",                                        null: false
     t.string   "password_digest",       limit: 255
     t.boolean  "admin",                             default: false
+    t.boolean  "check"
+    t.integer  "want_book",             limit: 4
+    t.string   "ISBN",                  limit: 255
   end
 
+  add_foreign_key "books", "users"
 end
